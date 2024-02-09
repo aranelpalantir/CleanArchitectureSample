5813860c8468edae96879177be44b40e98b21273
aranelpalantir <trmert@hotmail.com>
2024-02-09 16:14:43 +0300

    IdentityDbContext, User ve Role yapısı oluşturulması

--- Infrastructure/CleanArchSample.Persistence/Migrations/20240209131201_AddIdentityDbContext.Designer.cs
+++ Infrastructure/CleanArchSample.Persistence/Migrations/20240209131201_AddIdentityDbContext.Designer.cs
@@ -4,6 +4,7 @@ using CleanArchSample.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchSample.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240209131201_AddIdentityDbContext")]
+    partial class AddIdentityDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
