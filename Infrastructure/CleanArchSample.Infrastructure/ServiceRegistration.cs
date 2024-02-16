0a87c810ff34e1d8850d62148bff8b78a707b6a7
bravelight <trmert@hotmail.com>
2024-02-16 23:59:15 +0300

    add healthcheck

--- Infrastructure/CleanArchSample.Infrastructure/ServiceRegistration.cs
+++ Infrastructure/CleanArchSample.Infrastructure/ServiceRegistration.cs
@@ -55,6 +55,27 @@ namespace CleanArchSample.Infrastructure
                 var connectionMultiplexer = cfg.GetRequiredService<IConnectionMultiplexer>();
                 return connectionMultiplexer.GetDatabase();
             });
+
+            using var scope = serviceScopeFactory.CreateScope();
+            var redisCacheSettings = scope.ServiceProvider.GetRequiredService<IOptions<RedisCacheSettings>>().Value;
+
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("DefaultConnection")!)
+                .AddRedis(redisCacheSettings.Configuration);
+
+            services
+                .AddHealthChecksUI(setupSettings: setup =>
+                {
+                    setup.SetEvaluationTimeInSeconds(5); // Configures the UI to poll for healthchecks updates every 5 seconds
+                    setup.SetApiMaxActiveRequests(1);//Only one active request will be executed at a time. All the excedent requests will result in 429 (Too many requests)
+                    setup.AddHealthCheckEndpoint("endpoint1", "/health");
+                    setup.AddHealthCheckEndpoint("endpoint2", "http://localhost:8001/health");
+                    setup.AddHealthCheckEndpoint("endpoint3", "http://remoteendpoint:9000/health");
+                    setup.AddWebhookNotification("webhook1", uri: "https://healthchecks.requestcatcher.com/",
+                        payload: "{ message: \"Webhook report for [[LIVENESS]]: [[FAILURE]] - Description: [[DESCRIPTIONS]]\"}",
+                        restorePayload: "{ message: \"[[LIVENESS]] is back to life\"}");
+                    setup.SetNotifyUnHealthyOneTimeUntilChange(); // You will only receive one failure notification until the status changes.
+        }).AddInMemoryStorage();
         }
     }
 }
