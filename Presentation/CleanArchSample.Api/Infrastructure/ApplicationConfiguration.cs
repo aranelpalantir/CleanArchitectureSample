ab40d14c84b4249b760406622c60534b99df6fbb
aranelpalantir <trmert@hotmail.com>
2024-02-19 13:54:36 +0300

    refactor the program class

---
+++ Presentation/CleanArchSample.Api/Infrastructure/ApplicationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanArchSample.Api.Infrastructure
+{
+    public static class ApplicationConfiguration
+    {
+        public static void ConfigureSwagger(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "DockerCompose")
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Api v1");
+                    c.SwaggerEndpoint("/swagger/v2/swagger.json", "Web Api v2");
+                });
+            }
+        }
+    }
+}
